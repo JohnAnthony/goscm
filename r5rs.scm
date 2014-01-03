301e94a69c782398d4e1fbdee17e16fc39cd80af
John Anthony <john@jo.hnanthony.com>
2014-01-03 03:08:11 +0000

    Slight reshuffle of the checklist

--- r5rs.scm
+++ r5rs.scm
@@ -3,12 +3,3 @@
     (if b
         #f
         #t)))
-
-(define and
-  (lambda l
-    (if (null? l)
-        #t
-        (if (not (car l))
-            #f
-            (and (cdr l))))))
-            
