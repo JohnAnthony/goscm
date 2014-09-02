8e05f0f14ad0dca00a5725af62db7536adb59520
John Anthony <john@jo.hnanthony.com>
2014-09-02 22:59:56 +0100

    From-srcatch refresh for TDD

--- goscm_test.go
+++ goscm_test.go
@@ -6,10 +6,21 @@ import (
 
 func Test_Read(t *testing.T) {
 	// Numeric atom
-	inst := goscm.NewInstance()
-	v := inst.Read("2")
-	//if (v.Type != SCM_Number)
+	inst := NewInstance()
+	var ret *SCMCell
+	var remain string
+	
+	ret, remain = inst.Read("2")
+	//if (ret.Type != SCM_Number)
 	// error
-	//if (v.Value != 2)
+	//if (ret.Value != 2)
+	// error
+
+	ret, remain = inst.Read("2 3 4 5")
+	//if (ret.Type != SCM_Number)
+	// error
+	//if (ret.Value != 2)
+	// error
+	//if (remain != "3 4 5")
 	// error
 }
