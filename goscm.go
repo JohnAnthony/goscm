f09a2f99b0f227c96938157e546e24f4b3b348d7
John Anthony <john@jo.hnanthony.com>
2014-09-02 23:27:26 +0100

    Framework work

--- goscm.go
+++ goscm.go
@@ -7,9 +7,32 @@ type Instance struct {
 }
 
 type SCMCell struct {
-
 }
 
+// NewInstance takes a string description of a standard and returns a new scheme
+// interpreter instance initialized to meet that standard.
+// If the string description is nil, then we return a very minimal instance.
+func NewInstance(std string) *Instance {
+	return &Instance {
+	}
+}
+
+// Read takes a string and reads it using the instance's syntax rules.
+// It returns a *SCMCell of the first sexp it encounters and a string containing
+// the unprocessed remainder of the input string.
+// If EOF is reached while attempting to read an sexp, then an appropriate
+// scheme error is returned
 func (inst *Instance) Read(s string) (*SCMCell, string) {
 	return nil, ""
+}
+
+// Eval takes a *SCMCell and evaluates it inside of an environment
+func (inst *Instance) Eval(*SCMCell) *SCMCell {
+	return nil
+}
+
+// Print takes a *SCMCell and returns a string representation of that value
+// Note the this function does not actually print the value to the screen
+func (inst *Instance) Print(*SCMCell) string {
+	return ""
 }
--- goscm_test.go
+++ goscm_test.go
@@ -5,22 +5,49 @@ import (
 )
 
 func Test_Read(t *testing.T) {
-	// Numeric atom
-	inst := NewInstance()
+	inst := NewInstance(nil)
 	var ret *SCMCell
 	var remain string
 	
+	// Numbers //
+
 	ret, remain = inst.Read("2")
 	//if (ret.Type != SCM_Number)
 	// error
-	//if (ret.Value != 2)
+	// if (ret.Value != 2)
+	// error
+	// if (remain != "")
 	// error
 
 	ret, remain = inst.Read("2 3 4 5")
-	//if (ret.Type != SCM_Number)
+	// if (ret.Type != SCM_Number)
 	// error
-	//if (ret.Value != 2)
+	// if (ret.Value != 2)
 	// error
-	//if (remain != "3 4 5")
+	// if (remain != " 3 4 5")
+	// error
+	
+	// Lists //
+
+	ret, remain = inst.Read("(1 2 3)")
+	// if (ret.Type != SCM_List)
+	// error
+	// if (ret.Value.Car.Type != SCM_Number)
+	// error
+	// if (ret.Value.Car.Value != 1)
+	// error
+	// if (ret.Value.Cdr.Type != SCM_List)
+	// error
+	// if (ret.Value.Cdr.Value.Car.Type != SCM_Number)
+	// error
+	// if (ret.Value.Cdr.Value.Car.Value != 2)
+	// error
+	// if (ret.Value.Cdr.Value.Cdr.Type != SCM_List)
+	// error
+	// if (ret.Value.Cdr.Value.Cdr.Value.Car.Type != SCM_Number)
+	// error
+	// if (ret.Value.Cdr.Value.Cdr.Value.Car.Value != 3)
+	// error
+	// if (ret.Value.Cdr.Value.Cdr.Value.Cdr != nil)
 	// error
 }
