3f1e749ab57fbfb77a9e6f96e770de1679256985
John Anthony <john@jo.hnanthony.com>
2014-09-03 19:37:55 +0100

    Added new test

--- goscm_test.go
+++ goscm_test.go
@@ -16,16 +16,17 @@ func Test_Read_Integer(t *testing.T) {
 	if ret.Type != SCM_Integer {
 		t.Error("Expected to be of type SCM_Integer")
 	} else if *ret.Value.(*int) != 2 {
-    	t.Error("Expected returned value to be 2")
+    	t.Error("Expected returned value to be 2, got", *ret.Value.(*int))
 	} else if remain != "" {
-    	t.Error("Expected no remainder")
+    	t.Error("Expected no remainder, got", remain)
 	}
 
-	// ret, remain = inst.Read("2 3 4 5")
-	// if (ret.Type != SCM_Integer)
-	// error
-	// if (ret.Value != 2)
-	// error
-	// if (remain != " 3 4 5")
-	// error
+	ret, remain = inst.Read("90 30 40 50")
+	if ret.Type != SCM_Integer {
+		t.Error("Expected to be of type SCM_Integer")
+	} else if *ret.Value.(*int) != 90 {
+    	t.Error("Expected returned value to be 90, got", *ret.Value.(*int))
+	} else if remain != " 30 40 50" {
+		t.Error("Expected remainder to be \" 30 40 50\", got:", remain)
+	}
 }
