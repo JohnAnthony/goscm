dac7c1c53ff9913a3502b04bb04b17fec58a8513
John Anthony <john@jo.hnanthony.com>
2014-10-19 21:14:18 +0100

    Improved the quality of the tests

--- goscm_test.go
+++ goscm_test.go
@@ -2,27 +2,59 @@ package goscm
 
 import (
 	"testing"
-	"fmt"
+	"reflect"
 )
 
 func Test_Integer(t *testing.T) {
-	in := Make_SCMT(10)
-	fmt.Println(in.scm_print())
+	// A test with the integer 31337
+	in := Make_SCMT(31337)
+	if reflect.TypeOf(in).String() != "*goscm.SCMT_Integer" {
+		t.Error()
+	}
+	if in.(*SCMT_Integer).value != 31337 {
+		t.Error()
+	}
+	if in.scm_print() != "31337" {
+		t.Error()
+	}
 }
 
 func Test_String(t *testing.T) {
+	// A test with the string "foobar!"
 	str := Make_SCMT("foobar!")
-	fmt.Println(str.scm_print())
+	if reflect.TypeOf(str).String() != "*goscm.SCMT_String" {
+		t.Error()
+	}
+	if str.(*SCMT_String).value != "foobar!" {
+		t.Error()
+	}
+	if str.scm_print() != "\"foobar!\"" {
+		t.Error()
+	}
 }
 
 func Test_Pair(t *testing.T) {
-	pair := Cons(Make_SCMT("Foobarrrr!"), Make_SCMT(nil))
-	fmt.Println(pair.scm_print())
+	// A test with the singleton ("Foobarrrr!")
+	sing := Cons(Make_SCMT("Foobarrrr!"), Make_SCMT(nil))
+	if reflect.TypeOf(sing).String() != "*goscm.SCMT_Pair" {
+		t.Error()
+	}
+	if sing.scm_print() != "(\"Foobarrrr!\")" {
+		t.Error()
+	}
 	
-	pair2 := Cons(Make_SCMT(4), Make_SCMT(5))
-	fmt.Println(pair2.scm_print())
+	// A test with the pair (4 . 5)
+	pair := Cons(Make_SCMT(4), Make_SCMT(5))
+	if reflect.TypeOf(pair).String() != "*goscm.SCMT_Pair" {
+		t.Error()
+	}
+	if pair.scm_print() != "(4 . 5)" {
+		t.Error()
+	}
 	
-	list := Cons(Make_SCMT(10), Make_SCMT(nil))
+	// A test with the list (1 2 3 4 5 6 7 8 9) aka
+	// (1 . (2 . (3 . (4 . (5 . (6 . (7 . (8 . (9 . ())))))))))
+	list := Make_SCMT(nil)
 	list = Cons(Make_SCMT(9), list)
 	list = Cons(Make_SCMT(8), list)
 	list = Cons(Make_SCMT(7), list)
@@ -32,5 +64,10 @@ func Test_Pair(t *testing.T) {
 	list = Cons(Make_SCMT(3), list)
 	list = Cons(Make_SCMT(2), list)
 	list = Cons(Make_SCMT(1), list)
-	fmt.Println(list.scm_print())
+	if reflect.TypeOf(list).String() != "*goscm.SCMT_Pair" {
+		t.Error()
+	}
+	if list.scm_print() != "(1 2 3 4 5 6 7 8 9)" {
+		t.Error()
+	}
 }
