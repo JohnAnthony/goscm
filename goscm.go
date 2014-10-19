01c42feb3f3b1f9f60311d4bb8f42ff9d58e6ff1
John Anthony <john@jo.hnanthony.com>
2014-10-20 00:06:19 +0100

    Moved "utils" into the main package file

--- goscm.go
+++ goscm.go
@@ -1,9 +1,24 @@
 package goscm
 
-import (
-)
+import "reflect"
 
 type SCMT interface {
 	scm_eval(*SCMT_Environment) SCMT
 	String() string
+}
+
+func Make_SCMT(in interface {}) SCMT {
+	if in == nil {
+		return &SCMT_Nil {}
+	}
+
+	switch reflect.TypeOf(in).Kind() {
+	case reflect.Int:
+		return &SCMT_Integer { value: in.(int) }
+	case reflect.String:
+		return &SCMT_String { value: in.(string) }
+	default:
+		// TODO: We probably need to put an error here
+		return nil
+	}
 }
