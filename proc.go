bca392aa1cf6afee1dc46b5f2d7a75ede3ebd5b8
John Anthony <john@jo.hnanthony.com>
2014-11-01 17:16:09 +0000

    Added procedure framework

--- proc.go
+++ proc.go
@@ -1,3 +1,18 @@
 package goscm
 
-// TODO: This whole file/type
+type ECMT_Proc struct {
+	env *SCMT_Env
+	body *SCMT_Pair
+}
+
+func (p *SCMT_Proc) scm_eval(*SCMT_Env) SCMT {
+	return p
+}
+
+func (*SCMT_Proc) String() string {
+	return "#<procedure>"
+}
+
+func (p *SCMT_Proc) Apply(args, *SCMT_Pair, env *SCMT_Env) SCMT {
+	return SCMT_Nil
+}
