ed6a563fe831ca6c32375eab1ee8587f1088cd58
John Anthony <john@jo.hnanthony.com>
2014-11-04 19:26:26 +0000

    Separated out other reading types to functions

--- simple/read.go
+++ simple/read.go
@@ -32,51 +32,18 @@ func Read(b *bufio.Reader) (goscm.SCMT, *bufio.Reader) {
 	}
 	
 	switch {
-	case c == '(': // A list
-		list := goscm.SCMT_Nil
-		for {
-			c, err = b.ReadByte()
-			if c == ')' {
-				break
-			}
-			b.UnreadByte()
-			recurse, _ := Read(b)
-			list = goscm.Cons(recurse, list)
-		}
-		return goscm.Reverse(list), b
-	case c >= '0' && c <= '9': // An integer
-		ret := ""
-		for {
-			if err != nil {
-				break
-			}
-			if c == ')' {
-				b.UnreadByte()
-				break
-			}
-			if c == ';' {
-				read_comment(b)
-				continue
-			}
-			if is_whitespace(c) {
-				break
-			}
-			ret = string(append([]byte(ret), c))
-			c, err = b.ReadByte()
-		}
-		retn, _ := strconv.Atoi(ret)
-		return goscm.Make_SCMT(retn), b
-	case c == '"': // A string
-		ret := ""
-		for {
-			c, err = b.ReadByte()
-			if c == '"' {
-				break
-			}
-			ret = string(append([]byte(ret), c))
-		}
-		return goscm.Make_SCMT(ret), b
-	default: // A symbol
+	case c == '(': 
+		// A list
+		return read_list(b)
+	case c >= '0' && c <= '9':
+		// An integer
+		b.UnreadByte()
+		return read_integer(b)
+	case c == '"':
+		// A string
+		return read_string(b)
+	default:
+		// A symbol
 		b.UnreadByte()
 		return read_symbol(b)
 	}
@@ -121,4 +88,57 @@ func read_symbol(b *bufio.Reader) (goscm.SCMT, *bufio.Reader) {
 		ret = string(append([]byte(ret), c))
 	}
 	return goscm.Make_Symbol(ret), b
+}
+
+func read_string(b *bufio.Reader) (goscm.SCMT, *bufio.Reader) {
+	ret := ""
+	for {
+		c, err := b.ReadByte()
+		if err != nil {
+			break
+		}
+		if c == '"' {
+			break
+		}
+		ret = string(append([]byte(ret), c))
+	}
+	return goscm.Make_SCMT(ret), b
+}
+
+func read_integer(b *bufio.Reader) (goscm.SCMT, *bufio.Reader) {
+	ret := ""
+	for {
+		c, err := b.ReadByte()
+		if err != nil {
+			break
+		}
+		if c == ')' {
+			b.UnreadByte()
+			break
+		}
+		if c == ';' {
+			read_comment(b)
+			continue
+		}
+		if is_whitespace(c) {
+			break
+		}
+		ret = string(append([]byte(ret), c))
+	}
+	retn, _ := strconv.Atoi(ret)
+	return goscm.Make_SCMT(retn), b
+}
+
+func read_list(b *bufio.Reader) (goscm.SCMT, *bufio.Reader) {
+	list := goscm.SCMT_Nil
+	for {
+		c, _ := b.ReadByte()
+		if c == ')' {
+			break
+		}
+		b.UnreadByte()
+		recurse, _ := Read(b)
+		list = goscm.Cons(recurse, list)
+	}
+	return goscm.Reverse(list), b
 }
