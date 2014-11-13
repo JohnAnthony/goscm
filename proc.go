9a1b9d8b82a410038019d3f6a38af9d5c4320f52
John Anthony <john@jo.hnanthony.com>
2014-11-13 23:59:36 +0000

    Got TCO working!

--- proc.go
+++ proc.go
@@ -20,23 +20,56 @@ func (*SCMT_Proc) String() string {
 }
 
 func (p *SCMT_Proc) Apply(args *SCMT_Pair, env *SCMT_Env) (SCMT, error) {
-//TCO_TOP:
+	var err error
+
+TCO_TOP:
 	for {
-		args, err := MapEval(args, env)
+		args, err = MapEval(args, env)
+		if err != nil {	return SCMT_Nil, err }
+
 		newenv := EnvEmpty(env)
 		newenv.AddArgs(p.args, args)
 		body, err := p.body.ToSlice()
-		if err != nil {
-			return SCMT_Nil, err
-		}
+		if err != nil {	return SCMT_Nil, err }
 
 		for i := 0; i < len(body); i++ {
-			body[i], err = body[i].Eval(newenv)
-			if err != nil {
-				return SCMT_Nil, err
+			
+			// This is the last call in the body. We might need to TCO
+			if i == len(body) - 1 {
+				for reflect.TypeOf(body[i]) == reflect.TypeOf(&SCMT_Pair{}) {
+					tail_pair := body[i].(*SCMT_Pair)
+					
+					if reflect.TypeOf(tail_pair.Car) != reflect.TypeOf(&SCMT_Symbol{}) {
+						break
+					}
+
+					tail_func, err := tail_pair.Car.(*SCMT_Symbol).Eval(newenv)
+					if err != nil { return SCMT_Nil, err }
+					
+					if reflect.TypeOf(tail_func) == reflect.TypeOf(&SCMT_Special{}) {
+						if tail_func.(*SCMT_Special).Expand != nil {
+							// TODO: Check that body[i].Cdr is a *SCMT_Pair
+							body[i], err = tail_func.(*SCMT_Special).Expand(body[i].(*SCMT_Pair).Cdr.(*SCMT_Pair), newenv)
+							if err != nil { return SCMT_Nil, err }
+							continue
+						}
+					}
+
+					if tail_func == p {
+						env = newenv
+						args = body[i].(*SCMT_Pair).Cdr.(*SCMT_Pair)
+						continue TCO_TOP
+					}
+
+					break
+				}
 			}
 
-			if i == len(body) - 1 { // The last value, return its result
+			// This is not the last expression in the body, continue normally
+			body[i], err = body[i].Eval(newenv)
+			if err != nil { return SCMT_Nil, err }
+			
+			if i == len(body) - 1 {
 				return body[i], nil
 			}
 		}
